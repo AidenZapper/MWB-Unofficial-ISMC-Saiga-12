f258dfba9fb773f1e202e27a508faa1b607a83f2
AidenZapper <110077984+AidenZapper@users.noreply.github.com>
2023-02-13 23:21:50 +0000

    misc

--- lua/weapons/mg_base/modules/attachmentss/ismcsaiga12/skins/attachment_vm_sh_saiga12_tan_skin.lua
+++ lua/weapons/mg_base/modules/attachmentss/ismcsaiga12/skins/attachment_vm_sh_saiga12_tan_skin.lua
@@ -10,5 +10,5 @@ local BaseClass = GetAttachmentBaseClass(ATTACHMENT.Base)
 function ATTACHMENT:Appearance(model, category)
     BaseClass.Appearance(self, model, category)
     if (SERVER) then return end
-	model:SetSkin(3)
+	model:SetSkin(2)
 end
